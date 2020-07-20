b92458600ce7372b8682afded6bad360bc7abc01
Dan <Daniel.Bullock@ontariotechu.net>
2020-07-20 13:04:39 -0400

    Updates

---
+++ precache-manifest.671bc6795d6b6a91bc5c0f380b64fd90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "855bd1bf298dbbd35516",
+    "url": "/2020IsOverParty/static/css/main.a7073295.chunk.css"
+  },
+  {
+    "revision": "855bd1bf298dbbd35516",
+    "url": "/2020IsOverParty/static/js/main.576a8e9d.chunk.js"
+  },
+  {
+    "revision": "e7265ac5407844c917f7",
+    "url": "/2020IsOverParty/static/js/runtime~main.59bd2fcc.js"
+  },
+  {
+    "revision": "63366e5654f7186c2faa",
+    "url": "/2020IsOverParty/static/js/2.1ec6a50c.chunk.js"
+  },
+  {
+    "revision": "d8fb0274db8900764b9f18d5bd2668c6",
+    "url": "/2020IsOverParty/static/media/logo.d8fb0274.svg"
+  },
+  {
+    "revision": "c07f925999c2cc466aa03318c67ab5c1",
+    "url": "/2020IsOverParty/index.html"
+  }
+];
